94fd51fc002e664637f6fb3f4aad5f0a6a4f746c
abdulwahid <abdulwahid126@gmail.com>
2024-03-30 01:32:22 +0500

    uplode to gitHub

---
+++ README.md
@@ -0,0 +1,2 @@
+# CLI-ATM-Program
+This is CLI base ATM program with the help of Typescript &amp; Node.js
---
+++ index.ts
@@ -0,0 +1,69 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance = 10000; //Dollers
+let myPin = 1122
+
+let pinAnswer = await inquirer.prompt(
+    [
+        {
+        name : "pin",
+        message : "Please enter your pin:",
+        type : "number", 
+        }
+    ]
+);
+if(pinAnswer.pin === myPin){
+    console.log("Correct Pin Code!!!");
+
+    let operationAns = await inquirer.prompt
+    ([
+        {
+        name : "operation",
+        message : "Please select a option",
+        type : "list",
+        choices : ["withdraw", "check balance","fast cash"],
+        }
+    ]);
+
+    if(operationAns.operation === "withdraw"){
+        let amountAns = await inquirer.prompt(
+            {
+                name: "withdrawAmount",
+                message: "enter your amount",
+                type: "number",
+            },
+        );
+        if(amountAns.withdrawAmount <= myBalance){
+            myBalance -= amountAns.withdrawAmount
+        console.log(`your remaining amount is: ${myBalance}`);
+        }
+        else{
+            console.log("InSufficient Balance");
+        }
+    }
+    if(operationAns.operation === "check balance"){
+        console.log(`your balance is: ${myBalance}`);    
+    }
+    else if(operationAns.operation === "fast cash"){
+        let fastCash = await inquirer.prompt({
+            name: "selectAmount",
+            message: "please choose a ammount",
+            type: "list",
+            choices: ["1000", "5000", "10000", "20000"],
+        });
+        if(fastCash.selectAmount <= myBalance){
+        myBalance -= fastCash.selectAmount
+        console.log(`your remaining amount is: ${myBalance}`);
+    }
+    else{
+        console.log("InSufficient Balance");
+    }
+    }
+}
+else{
+    console.log("Invelid Pin Code");
+}
+
+                          //"The End"!!!;
